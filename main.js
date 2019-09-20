1ac8cefcfd7a4f4811184a37a75c26f3f1f31e24
Kiaresko <kseniia.i@ideasoft.io>
2019-09-20 17:41:22 +0300

    lesson 7

---
+++ main.js
@@ -0,0 +1,107 @@
+
+function codePreview(id, func) {
+    document.getElementById(id).nextElementSibling.textContent = func;
+}
+
+// lesson 07
+
+function showDemo71() {
+    var container = document.getElementById('demo_7_1');
+    var el = container.appendChild(document.createElement('p'));
+    el.textContent = 'click me';
+    el.style.cursor = 'pointer';
+    el.addEventListener('click', (event)=> {
+        var img = container.appendChild(document.createElement('img'));
+        img.src = 'https://picsum.photos/200/200';
+        img.style = `transition: all 1s;
+                    cursor: pointer;
+                    width: 100px;
+                    height: auto;`
+
+        function scaleImg(event) {
+            event.target.style.width = '200px'
+        }
+
+        img.addEventListener('mouseover', scaleImg);
+        img.addEventListener('click', (event) => {
+            event.target.parentNode.removeChild(event.target)
+            img.removeEventListener('mouseover', scaleImg);
+        })
+    })
+}
+showDemo71();
+codePreview('demo_7_1', showDemo71);
+
+function showDemo72() {
+    var collection = ['main', 'section', 'header', 'article'];
+    var titles = ["first", "second", "third", "fourth"];
+
+    function over ( event ) {
+        event.target.style.backgroundColor = '#ffff0050'
+    }
+    function out ( event ) {
+        event.target.style.backgroundColor = '#ff00ff50'
+    }
+    function clickHandler ( event ) {
+        event.stopPropagation();
+        event.target.parentElement.removeChild(event.target);
+    }
+
+    var container = document.getElementById('demo_7_2')
+
+    titles.forEach (
+        function ( tag, index  ) {
+            var el = container.appendChild(document.createElement(collection[index]));
+            el.style = `padding: 50px;
+                        background-color: #ff00ff50;
+                        border: dotted 1px yellow;`;
+            el.title = titles[index];
+
+            el.addEventListener('mouseout', out);
+            el.addEventListener('mouseover', over);
+            el.addEventListener('click', clickHandler);
+
+            container = el
+        }
+    )
+}
+
+showDemo72();
+codePreview('demo_7_2', showDemo72);
+
+function showDemo73() {
+    var collection = ['main', 'section', 'header', 'article'];
+    var titles = [1, 2, 3, 4, 5, 6, 7];
+
+    function over ( event ) {
+        event.target.style.backgroundColor = '#ffff0050'
+    }
+    function out ( event ) {
+        event.target.style.backgroundColor = '#ff00ff50'
+    }
+    function clickHandler ( event ) {
+        event.stopPropagation();
+        event.target.parentElement.removeChild(event.target);
+    }
+
+    var container = document.getElementById('demo_7_2')
+
+    titles.forEach (
+        function ( tag, index  ) {
+            var el = container.appendChild(document.createElement(collection[index]));
+            el.style = `padding: 50px;
+                        background-color: #ff00ff50;
+                        border: dotted 1px yellow;`;
+            el.title = titles[index];
+
+            el.addEventListener('mouseout', out);
+            el.addEventListener('mouseover', over);
+            el.addEventListener('click', clickHandler);
+
+            container = el
+        }
+    )
+}
+
+showDemo73();
+codePreview('demo_7_3', showDemo73);
