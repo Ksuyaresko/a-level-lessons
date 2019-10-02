2c751dfc9ed8251fcdd9c90f4ad31df200908b36
Kiaresko <kseniia.i@ideasoft.io>
2019-10-02 10:27:34 +0300

    lesson 10

--- main.js
+++ main.js
@@ -177,7 +177,7 @@ function showDemo83() {
 showDemo83();
 codePreview('demo_8_3', showDemo83);
 
-// lesson 08
+// lesson 09
 
 function showDemo91() {
     function typeMessage ( message, velocity ) {
@@ -273,3 +273,141 @@ function showDemo93() {
 
 showDemo93();
 codePreview('demo_9_3', showDemo93);
+
+// lesson 10
+
+function showDemo101(id) {
+    let messages = [
+        "backspace",
+        "enter",
+        "shift",
+        "control",
+        "delete",
+        "space",
+        "subtract"
+    ]
+
+    var log = {};
+
+    var sendMessage = ( message, callback ) =>
+        setTimeout (
+            () => callback ( message ),
+            Math.random () * 7000
+        )
+
+    handler = (msg) => {
+        Object.assign(log, {[getKey()]: msg});
+        document.getElementById(id).innerText = log
+    };
+
+    messages.forEach (
+        message => sendMessage ( message, handler )
+    );
+
+    getKey = () => {
+        var key = new Date().toLocaleString().split(", ")[1];
+        return log [ key ] ? key + "[2]" : key
+    };
+
+    log.__proto__.toString = function() {
+        return JSON.stringify(this)
+    }
+}
+
+showDemo101('demo_10_1');
+codePreview('demo_10_1', showDemo101);
+
+function showDemo102(id) {
+    function User ( name ) {
+        this.name = name;
+        var presence = false;
+        Object.defineProperty(this, 'presence', {
+            get: () => presence,
+            set: pres => { presence = !!pres }
+        })
+    }
+
+    let user = new User ( "Ivan" );
+
+    const container = document.getElementById(id);
+    container.appendChild(document.createElement('div')).innerHTML = `user.presence: ${user.presence}`;
+
+    user.presence = "+";
+    container.appendChild(document.createElement('div')).innerHTML = `user.presence: ${user.presence}`;
+}
+
+showDemo102('demo_10_2');
+codePreview('demo_10_2', showDemo102);
+
+function showDemo103(id) {
+    function User(name = 'Guest', mail = 'email', url = User.getAvatar()) {
+        this.name = name;
+        this.mail = mail;
+        this.photoURL = url;
+    }
+
+    User.avatars = [
+        "https://pre00.deviantart.net/50f9/th/pre/i/2011/217/e/8/pikachu_2_by_nostalgiaattack-d45jd3i.png",
+        "https://cdn.diversityavatars.com/wp-content/uploads/2018/01/Vector-Smart-Object-5.png",
+        "https://cdn4.iconfinder.com/data/icons/user-avatar-flat-icons/512/User_Avatar-31-512.png",
+        "http://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-L3-icon.png",
+        "https://findicons.com/files/icons/1072/face_avatars/300/i05.png",
+        "http://www.iconarchive.com/download/i51043/hopstarter/halloween-avatars/Gomez.ico",
+        "http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/256/Zombie-2-icon.png",
+        "https://vignette.wikia.nocookie.net/yogscast/images/8/8a/Avatar_Turps_2015.jpg"
+    ]
+
+    User.getAvatar = function() {
+        return this.avatars.shift()
+    }
+
+    User.admin = {
+        photoURL: "https://i.pinimg.com/originals/3d/47/4f/3d474f82ff71595e8081f9a120892ae8.gif",
+        name: "admin"
+    }
+
+    Object.defineProperty(User.prototype, 'messageBox', (function () {
+        const container = document.getElementById(id);
+        container.querySelector('textarea').oninput = function (event) {
+            event.target.parentNode.querySelector('img').src =  User.admin.photoURL;
+            event.target.parentNode.querySelector('span').textContent =  User.admin.name;
+        }
+        return {
+            value: container
+        }
+    })())
+
+    User.prototype.write = function () {
+        this.messageBox.querySelector('textarea').value = `Hi from ${this.name}`;
+        this.messageBox.querySelector('img').src =  this.photoURL;
+        this.messageBox.querySelector('img').alt =  this.name;
+        this.messageBox.querySelector('span').textContent =  this.name
+    }
+
+    User.prototype.read = function () {
+        console.log(`${this.name} read ${this.messageBox.querySelector('textarea').value}`)
+    }
+
+
+    var users = [
+        new User ( "Иван" ),
+        new User ( 'Alex', "alex@gmail.com" ),
+        new User ( 'Bob', "bob777@gmail.com" ),
+        new User ( 'Dima', "dima888@gmail.com" ),
+        new User ( 'Fima', "fima999@gmail.com" )
+    ]
+
+    var k = 1;
+    users.forEach (
+        function ( user, index, allUsers ) {
+            setTimeout (
+                function () {
+                    user.write ( `Hello, I'm ${user.name}` )
+                }, 3000 * k++
+            )
+        }
+    )
+}
+
+showDemo103('demo_10_3')
+codePreview('demo_10_3', showDemo103);
